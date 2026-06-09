7cca774b440f4641757e39aca94edac792a89587
theskysid <23x10sid@gmail.com>
2026-06-09 11:47:20 +0530

    springboot project initialized

---
+++ vehicle_scheduler/vehicle_scheduler/src/main/java/com/theskysid/vehicle_scheduler/VehicleSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.theskysid.vehicle_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSchedulerApplication.class, args);
+	}
+
+}
